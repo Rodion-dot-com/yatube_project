4504bde76c9c0fd7b0cbdafa93de73ffa97e3cda
Родион Прошляков <proshlyakovrodion@yandex.ru>
2022-07-05 18:12:59 +0300

    Post and Group models have been created and registered. A custom admin panel has been created for the Post model.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,23 +1,28 @@
 import os.path
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Post, Group
 
 
 # Create your views here.
 def index(request):
-    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
     title = 'Это главная страница проекта Yatube'
     context = {
         'title': title,
+        'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title,
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
